04b5395c271be2ebcbd399a60bdba8e5b28c3c45
shunsuke-tamura <wwwxx4869@gmail.com>
2023-08-07 14:44:40 +0900

    update: 処理実装

--- parse-css/src/css.rs
+++ parse-css/src/css.rs
@@ -82,6 +82,14 @@ pub enum CSSValue {
     Keyword(String),
 }
 
+fn whitespaces<Input>() -> impl Parser<Input, Output = String>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    many::<String, _, _>(space().or(newline()))
+}
+
 pub fn parse(raw: &str) -> Stylesheet {
     rules()
         .parse(raw)
@@ -94,8 +102,7 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this");
-    (char(' ')).map(|_| vec![])
+    (whitespaces(), many(rule().skip(whitespaces()))).map(|(_, rules)| rules)
 }
 
 fn rule<Input>() -> impl Parser<Input, Output = Rule>
@@ -103,11 +110,16 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this");
-    (char(' '),).map(|_| Rule {
-        selectors: vec![],
-        declarations: vec![],
-    })
+    (
+        selectors().skip(whitespaces()),
+        char::char('{').skip(whitespaces()),
+        declarations().skip(whitespaces()),
+        char::char('}'),
+    )
+        .map(|(selectors, _, declarations, _)| Rule {
+            selectors: selectors,
+            declarations,
+        })
 }
 
 fn selectors<Input>() -> impl Parser<Input, Output = Vec<Selector>>
@@ -115,8 +127,10 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this");
-    (char(' '),).map(|_| vec![])
+    sep_by(
+        simple_selector().skip(whitespaces()),
+        char::char(',').skip(whitespaces()),
+    )
 }
 
 fn simple_selector<Input>() -> impl Parser<Input, Output = SimpleSelector>
@@ -124,8 +138,51 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this");
-    (char(' '),).map(|_| SimpleSelector::UniversalSelector)
+    let universal_selector = char::char('*').map(|_| SimpleSelector::UniversalSelector);
+    let class_selector =
+        (char::char('.'), many1(letter())).map(|(_, class_name)| SimpleSelector::ClassSelector {
+            class_name: class_name,
+        });
+    let type_or_attribute_selector = (
+        many1(letter()).skip(whitespaces()),
+        optional((
+            char::char('[').skip(whitespaces()),
+            many1(letter()),
+            choice((char::string("="), char::string("~="))),
+            many1(letter()),
+            char::char(']'),
+        )),
+    )
+        .and_then(|(tag_name, opts)| match opts {
+            Some((_, attribute, op, value, _)) => {
+                let op = match op {
+                    "=" => AttributeSelectorOp::Eq,
+                    "~=" => AttributeSelectorOp::Contain,
+                    _ => {
+                        return Err(<Input::Error as combine::error::ParseError<
+                            char,
+                            Input::Range,
+                            Input::Position,
+                        >>::StreamError::message_static_message(
+                            "invalid attribute selector op",
+                        ))
+                    }
+                };
+                Ok(SimpleSelector::AttributeSelector {
+                    tag_name: tag_name,
+                    attribute: attribute,
+                    op: op,
+                    value: value,
+                })
+            }
+            None => Ok(SimpleSelector::TypeSelector { tag_name: tag_name }),
+        });
+
+    choice((
+        universal_selector,
+        class_selector,
+        type_or_attribute_selector,
+    ))
 }
 
 fn declarations<Input>() -> impl Parser<Input, Output = Vec<Declaration>>
@@ -133,8 +190,10 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this");
-    (char(' '),).map(|_| vec![])
+    sep_end_by(
+        declaration().skip(whitespaces()),
+        char::char(';').skip(whitespaces()),
+    )
 }
 
 fn declaration<Input>() -> impl Parser<Input, Output = Declaration>
@@ -142,11 +201,12 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this");
-    (char(' '),).map(|_| Declaration {
-        name: "".into(),
-        value: CSSValue::Keyword("".into()),
-    })
+    (
+        many1(letter()).skip(whitespaces()),
+        char::char(':').skip(whitespaces()),
+        css_value(),
+    )
+        .map(|(k, _, v)| Declaration { name: k, value: v })
 }
 
 fn css_value<Input>() -> impl Parser<Input, Output = CSSValue>
@@ -154,8 +214,8 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this");
-    (char(' '),).map(|_| CSSValue::Keyword("".into()))
+    let keyword = many1(letter()).map(|s| CSSValue::Keyword(s));
+    keyword
 }
 
 #[cfg(test)]
